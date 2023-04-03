3e8a7989c7223ac7729b1b8abd25d84b87e51ba5
Ahsoffice <ahsoffice8@gmail.com>
2023-04-03 22:53:40 +0500

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -29,7 +29,12 @@ if(shopParams.search) params =params.append('search',shopParams.search);
   return this.http.get<Pagination<Product[]>>(this.baseUrl + 'product',{params});
 }
 
+getProduct(id: number){
+  return this.http.get<Product>(this.baseUrl + 'product/'+ id);
+}
+
 getBrands()
+
 {
 
   return this.http.get<Brand[]>(this.baseUrl  +'product/brands');
